5a9c4fd02af86790fb86ea788aaf4c42299ddef0
anubhav0821 <124450638+anubhav0821@users.noreply.github.com>
2023-04-30 10:06:11 +0000

    added few colour changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,11 +12,11 @@ const Hero = () => {
                 Github
             </button>
         </nav>
-        <h1 className='head_text'>Summarize Articles with 
+        <h1 className='head_text'>Enter the link to summarize the articles with 
         <br className='max-md:hidden'/> <span className='orange_gradient'>OpenAI GPT-4</span>
         </h1>
         <h2 className='desc'>
-            Simplyfiy the article you want to read with Summize, an open source article summarizer that transforms leangthy articles into clear and consise summaries.
+        Summarize articles effortlessly with our innovative tool that leverages Chat GPT technology. Extract only the most important information from lengthy articles and enjoy clear and concise summaries.
         </h2>
     </header>
   )
